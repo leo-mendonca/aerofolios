fa5286928599eb2ba409b52237d7451804d9f278
leo-mendonca <leonardoxmendonca@gmail.com>
2024-01-13 10:35:39 -0300

    Definicao de arquivo para configuracao rapida do servidor

---
+++ Instalacao/configuracao.bat
@@ -0,0 +1,6 @@
+sudo apt update
+#!sudo apt install python
+sudo apt install python3-pip
+pip install -r "Requisitos EF.txt"
+mkdir -p "Saida/Aerofolio Fino NACA4"
+mkdir -p "Saida/Elementos Finitos NACA4"
