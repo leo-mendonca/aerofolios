2f03e558f91b87a0a5ccda0901e3e2494cb6a3f2
leo-mendonca <leonardoxmendonca@gmail.com>
2024-01-14 01:52:42 -0300

    Correcao de linha do configuracao.sh

--- Instalacao/configuracao.sh
+++ Instalacao/configuracao.sh
@@ -5,6 +5,6 @@ sudo apt install python3-pip
 pip install -r "Requisitos EF.txt"
 mkdir -p "../Saida/Aerofolio Fino NACA4"
 mkdir -p "../Saida/Elementos Finitos NACA4"
-mkdir -p "Malha"
-mkdir -p "Saida/MEF_NACA4"
+mkdir -p "../Malha"
+mkdir -p "../Saida/MEF_NACA4"
 sudo apt install gmsh
